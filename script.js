87958b3ab2723114653e4fcba1d8ddd3f925df27
Chandrachud <66238180+chandrasahi10@users.noreply.github.com>
2023-08-30 11:38:41 +0530

    Made functions

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-var clutter = ""
+var timer=60;
+var score=0;
+
+function makeBubble(){
+    var clutter = ""
 
 for (var i=1;i<=152;i++){
-    clutter += `<div class="bubble">5</div>`
+    rn = Math.floor(Math.random()*10);
+    clutter += `<div class="bubble">${rn}</div>`
 }
 
 document.querySelector('#pbtm').innerHTML = clutter;
+}
+
+
+function runTimer(){
+    var timerInt = setInterval(()=>{
+      if(timer>=0){
+      document.querySelector('#TimerValue').innerHTML = timer;
+      timer--;
+      }else{
+        clearInterval(timerInt);
+      }
+    },1000);  
+} 
+
+function getNewHit(){
+    var rn = Math.floor(Math.random()*10);
+    document.querySelector('#hitVal').textContent = rn;
+}
+
+function increaseScore(){
+    score+=10;
+    document.querySelector('#scoreVal').textContent = score;
+}
+
+runTimer();
+makeBubble();
+getNewHit();
